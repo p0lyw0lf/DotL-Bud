05c4710ed9c39918c9d92f55ac8b854efcf9495f
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2025-06-14 09:17:40 -0400

    fix rss checker

--- modules/rss_checker.py
+++ modules/rss_checker.py
@@ -25,30 +25,38 @@ class RSSChecker(VariableCommands):
                         tag,
                         pin_message=False,
                         mention_role=None):
-        feed = feedparser.parse(url)
-        item = feed["items"][0]  # Most recent
+        feed = feedparser.parse(url) 
+        # bluesky returns items in unsorted order, so we need to sort by date manually
+        items = sorted(feed["items"], key=lambda item: item["published_parsed"], reverse=True)
+        item = items[0]  # Most recent
+
+        if not self.is_announceable(item):
+            return
+
+        current_link = item["link"].strip()
         dbitem = ("last_link_" + tag, )
-        # check announceable first, in case tag is added after it's posted
-        if self.is_announceable(
-                item) and item["link"].strip() != self.db[dbitem].strip():
-            log.info("new: " + str(item["link"]) + " old: " +
-                     str(self.db[dbitem]))
-            self.db[dbitem] = item["link"]
-            channel_obj = self.client.get_channel(channel)
+        last_link = self.db[dbitem].strip()
+        if current_link == last_link:
+            return
 
-            formatted_message = message_template.replace(
-                "%page%", item["link"])
+        log.info(f"new: {current_link} old: {last_link}")
+        self.db[dbitem] = current_link
+
+        channel_obj = self.client.get_channel(channel)
+
+        formatted_message = message_template.replace(
+            "%page%", item["link"])
+        formatted_message = formatted_message.replace(
+            "%description%",
+            item.get("description", "[missing description]"))
+
+        if not (mention_role is None):
             formatted_message = formatted_message.replace(
-                "%description%",
-                item.get("description", "[missing description]"))
-
-            if not (mention_role is None):
-                formatted_message = formatted_message.replace(
-                    "%mention%",
-                    channel_obj.guild.get_role(mention_role).mention)
-            message = await self.send_simple_message(
-                formatted_message, self.client.get_channel(channel))
-            if pin_message: await message.pin()
+                "%mention%",
+                channel_obj.guild.get_role(mention_role).mention)
+        message = await self.send_simple_message(
+            formatted_message, self.client.get_channel(channel))
+        if pin_message: await message.pin()
 
     async def delete_previous_pins(self, channel, cutoff_age):
         """
